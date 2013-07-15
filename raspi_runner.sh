60f79286963e5d8a86e0d1ce1745348c6e2ec50e
enkydu <enkydu@gmail.com>
2013-07-15 23:30:24 +0200

    raspi_runner.sh v1.01

--- raspi_runner.sh
+++ raspi_runner.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 #Author: Pavol Salgari
 #Twitter: @enkydu
-#Version: 1.00
+#Version: 1.01
+
+rr_home="/home/pi/Dropbox"
+rr_storage="/home/pi/Dropbox/Raspi_Commands"
+
+cd $rr_home 
 
 #Download new commands delivered by mail from Dropbox to Raspberry Pi folder /home/pi/Dropbox/Raspi_Commands
-/home/pi/Dropbox/dropbox_uploader.sh -q download Raspi_Commands
+$rr_home/dropbox_uploader.sh -q download /Raspi_Commands
 
 # Check for new files on Raspberry Pi 
-files=`ls /home/pi/Dropbox/Raspi_Commands`
+files=`ls $rr_storage`
 
 # Grant rights for executing of delivered commands
 for i in $files
 do
-	chmod +x /home/pi/Dropbox/Raspi_Commands/$i
+	chmod +x $rr_storage/$i
 done
 
 # Run all delivered commands
 for i in $files
 do
-	/home/pi/Dropbox/Raspi_Commands/$i 2>&1 > /dev/null
+	$rr_storage/$i 2>&1 > /dev/null
 done
 
 # Remove all commands, which were already executed from Dropbox
 for i in $files
 do
-	/home/pi/Dropbox/dropbox_uploader.sh -q remove /Raspi_Commands/$i
+	$rr_home/dropbox_uploader.sh -q remove /Raspi_Commands/$i
 done
 
 # Remove all commands, which were already executed from Raspberry Pi
-rm /home/pi/Dropbox/Raspi_Commands/*
+rm $rr_storage/*
